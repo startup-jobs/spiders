1843ea6d3b8dc628c31ea801ebe423c6acb00c0c
Robert George <robert@justeat.in>
2015-04-28 18:17:21 +0530

    Refs #1 	Updated job item definition

--- jobs/jobs/spiders/hasjob.py
+++ jobs/jobs/spiders/hasjob.py
@@ -9,15 +9,16 @@ class HasJobSpider(CrawlSpider):
 	start_urls = ['https://hasjob.co']
 	rules = [Rule(LinkExtractor(restrict_xpaths='//*[@id="stickie-area"]/li', tags='a'), 'parse_jobs')]
 	
+	#http://stackoverflow.com/questions/8798235/dynamic-start-urls-in-scrapy
 	def parse_jobs(self, response):
 		torrent = JobsItem()
-		torrent['title'] = response.css('h1[itemprop="name"]::text').extract();
-		torrent['location'] = response.css('#post-location span[itemprop="name"]::text').extract();
+		torrent['title'] = response.css('h1[itemprop="name"]::text').extract()[0];
+		torrent['location'] = response.css('#post-location span[itemprop="name"]::text').extract()[0];
 		torrent['post_url'] = response.url
-		torrent['organization'] = response.css('strong[itemprop="name"]::text').extract();	
-		torrent['organization_url']  = response.css('#post-location + a::text').extract();
-		torrent['job_type'] = response.css('a[href^="/type"]::text').extract();
-		torrent['job_category'] = response.css('a[href^="/category"]::text').extract();
+		torrent['organization'] = response.css('strong[itemprop="name"]::text').extract()[0];
+		torrent['organization_url']  = response.css('#post-location + a::text').extract()[0];
+		torrent['job_type'] = response.css('a[href^="/type"]::text').extract()[0];
+		torrent['job_category'] = response.css('a[href^="/category"]::text').extract()[0];
 		torrent['salary'] = ''
 		
 		
